e2fdd50dd139a11ddfd0059d756e52a27df98c8f
Kirill D <kddreauw@gmail.com>
2023-05-05 22:37:23 +1000

    fix a bug where going back is handled by htmx and event handlers get dropped from the page

--- backend/blog/static/js/htmx_helpers.js
+++ backend/blog/static/js/htmx_helpers.js
@@ -1,12 +1,23 @@
-// select_htmx
-document.addEventListener('DOMContentLoaded', function () {
-  // onchange="this.setAttribute('hx-push-url', '{{ category.get_absolute_url }}' + (this.value !== '' ? ('?tag=' + this.value) : ''));"
-  const selectElement = document.getElementById('cat_tag_select');
-  if (selectElement) {
+(function () {
+  // select_htmx - push url
+  // this fix is necessary because htmx for select element doesn't work on children option items
+  // so we need to refresh hx-target for the parent select element every time a new choice is made
+  function selectFilterHtmxPushFix() {
+    const selectElement = document.getElementById('cat_tag_select');
+    // when the filter selected option changes, it becomes 'value' for the parent select element
     selectElement.addEventListener('change', function () {
+      // we preload path to the category for this filter
       let url = this.dataset.url;
+      // then if the new value is empty, push url without ?tag=
       if (this.value) url += '?tag=' + this.value;
       this.setAttribute('hx-push-url', url);
     });
   }
-});
+
+  function addListenersOnPageLoadAndAttachToHtmx() {
+    selectFilterHtmxPushFix();
+    document.body.addEventListener("htmx:historyRestore", selectFilterHtmxPushFix);
+  }
+
+  document.addEventListener('DOMContentLoaded', addListenersOnPageLoadAndAttachToHtmx);
+})();
--- backend/blog/static/js/post_helpers.js
+++ backend/blog/static/js/post_helpers.js
@@ -1,14 +1,18 @@
-window.addEventListener('load', () => {
-
+(function () {
+    // when there are too many tags in a post, expand them using a 'Show all' button
     function expandTagsInParentEl() {
         this.parentElement.querySelectorAll(".is-hidden").forEach(elem => elem.classList.remove("is-hidden"));
         this.remove();
     }
 
-    let tagExpanderElems = document.querySelectorAll("a.tag-expander");
-    tagExpanderElems.forEach(elem => elem.addEventListener("click", expandTagsInParentEl));
+    function addListenersOnPageLoadAndAttachToHtmx() {
+        const tagExpanderElems = document.querySelectorAll("a.tag-expander");
+        tagExpanderElems.forEach(elem => elem.addEventListener("click", expandTagsInParentEl));
 
-    htmx.onLoad(function (content) {
-        content.querySelectorAll("a.tag-expander").forEach(elem => elem.addEventListener("click", expandTagsInParentEl));
-    });
-});
+        htmx.onLoad(function (content) {
+            content.querySelectorAll("a.tag-expander").forEach(elem => elem.addEventListener("click", expandTagsInParentEl));
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', addListenersOnPageLoadAndAttachToHtmx);
+})();
