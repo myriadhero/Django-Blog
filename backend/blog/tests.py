b85013437f878eb1aae61adbd2b511662baaf932
Kirill Duplyakin <kduplyakin@atlassian.com>
2023-08-12 14:45:42 +1000

    start tests

--- backend/blog/tests.py
+++ backend/blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+# front page
+# category pages, tag list and blog list
+# tag page
+# all posts page
+# search page
+# pagination for all above (except front)
+# post detail page
+# about page
+# admin pages - queries?
+
+
+class FrontPageTests(TestCase):
+    def setUp(self) -> None:
+        url = "/"
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
