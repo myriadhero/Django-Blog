735103cb38baeb360bf8f9b63b8cee213e43b9da
Kirill D <kddreauw@gmail.com>
2023-11-15 00:48:00 +1100

    hide advanced search fields in search page by default

--- backend/blog/forms.py
+++ backend/blog/forms.py
@@ -1,10 +1,17 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import HTML, Div, Field, Layout, Submit
 from django import forms
 
 from .models import Category, CategoryTag, Post, Subcategory
 
 
 class AdvancedSearchForm(forms.Form):
-    query = forms.CharField(required=False, strip=True, max_length=200)
+    query = forms.CharField(
+        required=False,
+        strip=True,
+        max_length=100,
+        label=False,
+    )
     categories = forms.ModelMultipleChoiceField(
         Category.objects.all(), required=False, to_field_name="slug"
     )
@@ -25,6 +32,27 @@ class AdvancedSearchForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_action = "blog:post_search"
+        self.helper.form_method = "get"
+        self.helper.layout = Layout(
+            Field("query", title=""),
+            HTML(
+                """<div id="adv-search-expand" class="block">
+                Advanced search options <span class="icon"><i class="fa-solid fa-angle-up"></i></span>
+                </div>"""
+            ),
+            Div(
+                "categories",
+                "subcategories",
+                "tags",
+                "before",
+                "after",
+                css_class="collapsed block",
+                css_id="adv-search",
+            ),
+            Submit("submit", "Search", css_class="button is-primary block"),
+        )
 
         if Post.published.exists():
             min_date = Post.published.order_by("publish").first().publish.date()
